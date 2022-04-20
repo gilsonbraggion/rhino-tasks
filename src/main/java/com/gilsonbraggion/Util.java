69f645a2b8e1da6ca6a9e97120eaa633a5195362
Gilson Braggion <gilson.graggion@gmail.com>
2022-04-20 00:07:39 -0300

    Ajuste Atividade

--- src/main/java/com/gilsonbraggion/Util.java
+++ src/main/java/com/gilsonbraggion/Util.java
@@ -2,13 +2,17 @@ package com.gilsonbraggion;
 
 import java.util.Date;
 
+import org.apache.commons.lang3.time.DateUtils;
+
 import com.gilsonbraggion.model.Atividade;
 
 public class Util {
 
 	public static String getEstiloLinha(Atividade atividade) {
 
-		boolean contaVencida = atividade.getDataExecucao().compareTo(new Date()) < 0;
+		Date dataAtualPlus5 = DateUtils.addDays(new Date(), 5);
+		
+		boolean contaVencida = atividade.getDataExecucao().compareTo(dataAtualPlus5) < 0;
 
 		if (atividade.getDataFinalizacao() != null) {
 			return "atividadeConcluida";
