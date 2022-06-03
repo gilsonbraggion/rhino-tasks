fb76bc4d975b305cf8be79eb1529e8662006c233
Gilson Braggion <gilson.graggion@gmail.com>
2022-06-03 00:00:11 -0300

    Ajustes One on One

--- src/main/java/com/gilsonbraggion/service/MyCustomDetailService.java
+++ src/main/java/com/gilsonbraggion/service/MyCustomDetailService.java
@@ -9,6 +9,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import com.gilsonbraggion.model.Usuario;
 import com.gilsonbraggion.repository.UserRepository;
@@ -23,6 +25,9 @@ public class MyCustomDetailService implements UserDetailsService {
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		
 		Usuario user = userRepo.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Email " + username + " not found ")) ;
+		
+		ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+		attributes.getRequest().getSession().setAttribute("USUARIO_LOGGED", user);
 		
 		return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), getAuthorities(user));
 	}
